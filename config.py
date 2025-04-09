cb0f84f5f35832c24d72318b72172c7065112063
mautddos <ddosysy@hotmail.com>
2025-04-09 15:40:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+BOT_TOKEN = "8145114551:AAGOU9-3ZmRVxU91cPThM8vd932rNroR3WA"
+API_ID = 12345678  # Replace with your API ID
+API_HASH = "abcd1234efgh5678ijkl9012mnop3456"  # Replace with your API hash
